afba4c65608cddea7a9bea72c88506a074d5a68f
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-24 16:41:29 +0200

    Added neccessary project files

---
+++ choiceurl/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.secret_key = '\x8d-\x83\x1bD\xa6\xb7wt\xa3\xc3\xef\xbd\xd8\x90\xb5\x8cJ\xba\x04\xd7,\xf8S'
+
+    from . import choiceurl
+    app.register_blueprint(choiceurl.bp)
+
+    return app
