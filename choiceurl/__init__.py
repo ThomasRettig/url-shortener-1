62b0e2a58b23dde7b2459ad87c6237f564eef979
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-03-25 17:59:55 +0200

    Added descriptions

--- choiceurl/__init__.py
+++ choiceurl/__init__.py
@@ -1,9 +1,12 @@
+#import Flask
 from flask import Flask
 
+#create registerd app as a blueprint
 def create_app(test_config=None):
     app = Flask(__name__)
     app.secret_key = '\x8d-\x83\x1bD\xa6\xb7wt\xa3\xc3\xef\xbd\xd8\x90\xb5\x8cJ\xba\x04\xd7,\xf8S'
 
+    #import the app
     from . import choiceurl
     app.register_blueprint(choiceurl.bp)
 
