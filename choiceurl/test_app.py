07d26f113b3c233b91f1378f5bc7e6ec34ccf4b3
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-03-25 18:01:47 +0200

    Test the buttons on main page

--- choiceurl/test_app.py
+++ choiceurl/test_app.py
@@ -1,5 +1,6 @@
 from urlshort import create_app
 #perform one test on the shorten button
 def test_shorten(client):
+    #get the home page and test shorten button
     response = client.get('/')
     assert b'Shorten' in response.data
